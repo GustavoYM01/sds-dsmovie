26bc451a8c83bfa26308358353684d141e2f8fa0
GustavoYM01 <gumatimoto@hotmail.com>
2022-01-15 16:17:21 -0300

    Navbar github modified

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,5 +1,5 @@
-import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
-import './styles.css';
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg'
+import './styles.css'
 
 function Navbar() {
   return (
@@ -19,4 +19,4 @@ function Navbar() {
   )
 }
 
-export default Navbar;
+export default Navbar
